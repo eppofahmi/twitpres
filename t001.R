e6dedf9c07989b6ba8a77b766c5aa6caae1926d9
UJANG FAHMI <eppofahmi@gmail.com>
2020-02-10 18:14:24 +0700

    Dokumentasi nama kolom

--- t001.R
+++ t001.R
@@ -3,6 +3,7 @@ library(tidyverse)
 jokowi <- read_csv("Data/u-eppo q-from jokowi from 2018-09-21 to 2019-10-20.csv", 
                    trim_ws = FALSE)
 prabowo <- read_csv("Data/u-eppo q-from prabowo from 2018-09-21 to 2019-10-20.csv")
-glimpse(jokowi)
 
+
+colnames(jokowi)
 
